b223762f3d65b666bd20931c2e81965c1f66e0df
ssahin <ssahin@SEVGI-SAHIN.netas.lab.nortel.com>
2012-06-11 15:05:34 +0300

    Comment is added to person

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -23,6 +23,7 @@ public class Person implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    //test git diff
     private String userName;
     private String password;
     private String phoneNumber;
